6900e8b9a199245b9d4f4bac5e9d8b2abf4f2177
SuyeonYoo <36914099+SuyeonYoo@users.noreply.github.com>
2020-03-29 01:35:35 +0900

    first commit

---
+++ src/test/java/io/shc/suyeon/springboot/ApplicationTests.java
@@ -0,0 +1,13 @@
+package io.shc.suyeon.springboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
