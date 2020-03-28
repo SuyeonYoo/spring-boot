0e4a3b033308d657082ee74cb57e7c8d66e1660a
SuyeonYoo <36914099+SuyeonYoo@users.noreply.github.com>
2020-03-29 05:03:50 +0900

    controller modify

--- src/main/java/io/shc/suyeon/springboot/api/BookController.java
+++ src/main/java/io/shc/suyeon/springboot/api/BookController.java
@@ -3,6 +3,7 @@ package io.shc.suyeon.springboot.api;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -17,7 +18,7 @@ public class BookController {
 	BookService bookService;
 	
 	@GetMapping("/{bookId}")
-	public ResponseEntity<Book> findById(Long bookId){
+	public ResponseEntity<Book> findById(@PathVariable Long bookId){
 		Book book = bookService.findById(bookId)
 				.orElseThrow(() -> new RuntimeException("Not Found: " + bookId));
 		return ResponseEntity.ok(book);
